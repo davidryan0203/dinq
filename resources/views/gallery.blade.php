789dfed26ecdd138628993b5909b7876611edc51
Ryan David <davidryan0203@gmail.com>
2021-03-08 20:23:25 +0800

    updates

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -12,7 +12,6 @@
             @foreach($results as $data)
                 @if($data['media'] != '')
                 <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                    {{$data['id']}}
                     <img class="img-thumbnail"
                          src="{{$data['media']}}"
                          alt="Another alt text">
